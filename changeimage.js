2356023f7c0ae26ffdac0e7a6fab4cdbc4bf94ee
Seanti <seanpizzaspark@gmail.com>
2024-05-14 14:22:53 +0800

    stratagem change image

---
+++ changeimage.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Stratagem change image
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-14
+// @description  try to take over the world!
+// @author       You
+// @match        https://combustibletoast.github.io/
+// @icon         https://ih0.redbubble.net/image.5470760030.3415/raf,360x360,075,t,fafafa:ca443f4786.jpg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const up_image = "./data/Images/Arrows/U.png";
+    const down_image = "./data/Images/Arrows/D.png";
+    const right_image = "./data/Images/Arrows/R.png";
+    const left_image = "./data/Images/Arrows/L.png";
+
+    
+
+
+
+})();
